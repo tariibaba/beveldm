fbf91fab389f7841c28d9565393780ef71e3b1b8
Tari Ibaba <tariibabadev@gmail.com>
2019-10-03 21:41:00 +0100

    Wrap App element in react-redux Provider

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { downloads } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(combineReducers({ downloads }));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
