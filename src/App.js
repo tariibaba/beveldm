e629ee451a99b400093b7e4e152398f65158c9f3
Tari Ibaba <tariibabadev@gmail.com>
2019-10-03 22:25:39 +0100

    Remove constructor

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import AddNewDownload from './components/AddNewDownload';
 import DownloadList from './components/DownloadList';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      downloads: []
-    };
-  }
-
   render() {
     return (
       <div className="App">
         <AddNewDownload />
-        <DownloadList downloads={this.state.downloads} />
+        <DownloadList />
       </div>
     );
   }
