a4f3231f29bc83a3d4b3f3ef64ef01b4619ead0f
Tari Ibaba <tariibabadev@gmail.com>
2019-10-10 00:57:42 +0100

    Make ProgressBar only update at percentage lengths

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ProgressBar.css';
 
-function ProgressBar({ value }) {
-  const width = 200;
-  let progressWidth = width * value;
-  if (!value || value < 0)
-    progressWidth = 0;
-  if (value > 1)
-    progressWidth = width;
-  return (
-    <div className="ProgressBar-progress-bar" style={{ width: width+'px' }}>
-      <div className="ProgressBar-progress" style={{ width: progressWidth }}></div>
-    </div>
-  );
+class ProgressBar extends Component {
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.value - this.valueBeforeUpdate > 0.01) {
+      this.valueBeforeUpdate += 0.01;
+      return true;
+    }
+    return false;
+  }
+
+  fractionFloor(num, fractionDigits) {
+    const powerOf10 = Math.pow(10, fractionDigits);
+    return Math.floor(num) + Math.floor(num * powerOf10) / powerOf10;
+  }
+
+  render() {
+    this.valueBeforeUpdate = this.fractionFloor(this.props.value, 2);
+    const width = 200;
+    let progressWidth = width * this.valueBeforeUpdate;
+    if (!this.valueBeforeUpdate || this.valueBeforeUpdate < 0)
+      progressWidth = 0;
+    if (this.valueBeforeUpdate > 1) progressWidth = width;
+
+    return (
+      <div className="ProgressBar-progress-bar" style={{ width: width + 'px' }}>
+        <div
+          className="ProgressBar-progress"
+          style={{ width: progressWidth }}
+        ></div>
+      </div>
+    );
+  }
 }
 
 export default ProgressBar;
