c88c27eab38b73f397ff6604cd9fd7b9d342586d
Tari Ibaba <tariibabadev@gmail.com>
2019-10-24 21:29:04 +0100

    Check download status before continuing

--- src/thunks.js
+++ src/thunks.js
@@ -47,6 +47,7 @@ export function thunkStartDownload(id) {
 export function thunkPauseDownload(id) {
   return (dispatch, getState) => {
     const download = getState().downloads.find(download => download.id === id);
+    if (download.status !== 'started') return;
     download.res.pause();
     dispatch(pauseDownload(id));
   };
