732010ef0bf472fb97f668bfa25471d834fceabd
Tari Ibaba <tariibabadev@gmail.com>
2019-11-19 23:02:08 +0100

    Format code

--- public/main.js
+++ public/main.js
@@ -26,14 +26,14 @@ function createWindow() {
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
   });
 
-  mainWindow.on('close', (e) => {
+  mainWindow.on('close', e => {
     e.preventDefault();
     mainWindow.webContents.send('close', null);
   });
@@ -49,13 +49,13 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('activate', function () {
+app.on('activate', function() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow();
@@ -66,7 +66,5 @@ app.on('activate', function () {
 ipcMain.on('choose-file', event => {
   dialog
     .showOpenDialog(mainWindow, { properties: ['openDirectory'] })
-    .then(value => 
-      event.sender.send('choosen-file', value.filePaths[0])
-    );
+    .then(value => event.sender.send('choosen-file', value.filePaths[0]));
 });
