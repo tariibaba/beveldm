76b698e751035bd6e9676cc107912b57a12bfaf5
Tari Ibaba <tariibabadev@gmail.com>
2019-11-24 15:35:44 +0100

    Add more cleanup logic to thunk

--- src/thunks/pause-download.js
+++ src/thunks/pause-download.js
@@ -3,8 +3,8 @@ import { pauseDownload } from '../actions';
 export default function thunkPauseDownload(id) {
   return (dispatch, getState) => {
     const download = getState().downloads.find(download => download.id === id);
-    if (download.status !== 'started') return;
-    download.res.pause();
+    if (download.res) download.res.pause();
     dispatch(pauseDownload(id));
+    return Promise.resolve();
   };
 }
--- src/thunks/save-state.js
+++ src/thunks/save-state.js
@@ -1,10 +1,16 @@
 import thunkPauseDownload from './pause-download';
 import Store from 'electron-store';
+import { deleteFile, getDownloadPath } from './helpers';
+import { downloadNotStarted } from '../actions';
 
 export default function saveState() {
   return async (dispatch, getState) => {
     getState().downloads.forEach(async download => {
-      await dispatch(thunkPauseDownload(download.id));
+      if (download.status === 'starting')
+        dispatch(downloadNotStarted(download.id));
+      if (download.status === 'resuming' || download.status === 'started')
+        await dispatch(thunkPauseDownload(download.id));
+      if (!download.show) deleteFile(getDownloadPath(download));
     });
     const store = new Store();
     return new Promise(resolve => {
@@ -12,7 +18,12 @@ export default function saveState() {
         store.set(
           'downloads',
           getState()
-            .downloads.filter(download => download.status !== 'gettinginfo')
+            .downloads.filter(
+              download =>
+                download.status !== 'gettinginfo' &&
+                download.status !== 'deleted' &&
+                download.show
+            )
             .map(download => {
               const { res, ...d } = { ...download };
               return d;
