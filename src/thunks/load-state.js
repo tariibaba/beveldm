9dfd2ce4ea0f3ba2c7c1508c6a781a1e0e237b0c
Tari Ibaba <tariibabadev@gmail.com>
2019-12-09 21:57:40 +0100

    Get bytes downloaded from file size
    Get bytes downloaded from file size in case of lag

--- src/thunks/load-state.js
+++ src/thunks/load-state.js
@@ -1,9 +1,20 @@
 import Store from 'electron-store';
+import { updateBytesDownloaded } from '../actions';
+import fs from 'fs';
+import { getPartialDownloadPath } from './helpers';
 
 export default function loadState() {
-  return async (_dispatch, getState) => {
+  return async (dispatch, getState) => {
     const store = new Store();
     getState().downloads = store.get('downloads') || [];
+    getState().downloads.forEach(download => {
+      if (download.status === 'paused') {
+        const partialPath = getPartialDownloadPath(download);
+        dispatch(
+          updateBytesDownloaded(download.id, fs.statSync(partialPath).size)
+        );
+      }
+    });
     getState().settings = store.get('settings');
   };
 }
