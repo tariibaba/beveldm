884ca463aa7198379da9ab7dac8148607d6b33b8
Tari Ibaba <tariibabadev@gmail.com>
2019-12-12 22:31:58 +0100

    Move rescripts config to .rescriptsrc.js

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,1 +1,9 @@
-module.exports = [require.resolve('./.webpack.config.js')];
+module.exports = config => {
+  config.target = 'electron-renderer';
+  config.module.rules.push({
+    test: /\.(woff|woff2|eot|ttf|otf)$/,
+    use: 'file-loader'
+  });
+
+  return config;
+};
--- .webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = config => {
-  config.target = 'electron-renderer';
-  config.module.rules.push({
-    test: /\.(woff|woff2|eot|ttf|otf)$/,
-    use: 'file-loader'
-  });
-
-  return config;
-};
