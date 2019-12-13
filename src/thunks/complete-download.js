1705d90044d8fdff28b5fc50024d634c8481ad77
Tari Ibaba <tariibabadev@gmail.com>
2019-12-13 13:55:30 +0100

    Refactor code

--- src/thunks/complete-download.js
+++ src/thunks/complete-download.js
@@ -1,22 +1,15 @@
-import { replaceFileExt } from './helpers';
-import { PARTIAL_DOWNLOAD_EXTENSION } from '../constants';
+import { getPartialDownloadPath, getDownloadPath } from './helpers';
 import pify from 'pify';
-import path from 'path';
 import { completeDownload } from '../actions';
 import fs from 'fs';
 
 export default function thunkCompleteDownload(id) {
   return async (dispatch, getState) => {
-    const rename = pify(fs.rename, { multiArgs: true });
     const download = getState().downloads.find(download => download.id === id);
-    await rename(
-      replaceFileExt(
-        path.join(download.dirname, download.availableFilename),
-        PARTIAL_DOWNLOAD_EXTENSION
-      ),
-      path.join(download.dirname, download.availableFilename)
-    );
+    const rename = pify(fs.rename, { multiArgs: true });
+    await rename(getPartialDownloadPath(download), getDownloadPath(download));
     dispatch(completeDownload(id));
+
     return Promise.resolve();
   };
 }
