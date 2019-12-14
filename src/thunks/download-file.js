5f8e39a0cd82891ddfcbe68682c19529cb171251
Tari Ibaba <tariibabadev@gmail.com>
2019-12-14 14:22:03 +0100

    Exhaust buffer below half of SAVE_DATA_LIMIT before resuming request

--- src/thunks/download-file.js
+++ src/thunks/download-file.js
@@ -5,40 +5,63 @@ import { SAVE_DATA_LIMIT } from '../constants';
 
 export default function thunkDownloadFile(id, res) {
   return async (dispatch, getState) => {
-    let download = getState().downloads.find(download => download.id === id);
+    let state = getState();
+    let download = state.downloads.find(download => download.id === id);
+
     const partialDownloadPath = getPartialDownloadPath(download);
     const fileStream = fs.createWriteStream(partialDownloadPath, {
       flags: 'a'
     });
     let buffer;
     let speedThrottleTimeout;
+
     res
-      .on('data', chunk => {
+      .on('data', async chunk => {
         res.pause();
-        download = getState().downloads.find(download => download.id === id);
+
+        state = getState();
+        download = state.downloads.find(download => download.id === id);
+
         if (!buffer) {
           buffer = chunk;
         } else {
           buffer = Buffer.concat([buffer, chunk]);
         }
-        const saveData = getState().settings.saveData;
+        
+        const saveData = state.settings.saveData;
         if (saveData) {
-          const chunkToWrite = buffer.slice(0, SAVE_DATA_LIMIT / 2);
-          buffer = buffer.slice(SAVE_DATA_LIMIT / 2, buffer.length);
-          speedThrottleTimeout = setTimeout(() => {
-            fileStream.write(chunkToWrite, err => {
-              if (err) throw err;
-              const received = download.bytesDownloaded + chunkToWrite.length;
-              dispatch(thunkUpdateBytesDownloaded(id, received));
-              if (download.status !== 'paused') res.resume();
-            });
-          }, 500);
+          const writeSlicedBuffer = () => {
+            state = getState();
+            download = state.downloads.find(download => download.id === id);
+
+            const chunkToWrite = buffer.slice(0, SAVE_DATA_LIMIT / 2);
+            buffer = buffer.slice(SAVE_DATA_LIMIT / 2);
+
+            speedThrottleTimeout = setTimeout(() => {
+              fileStream.write(chunkToWrite, async err => {
+                if (err) throw err;
+                const received = download.bytesDownloaded + chunkToWrite.length;
+                dispatch(thunkUpdateBytesDownloaded(id, received));
+                
+                if (
+                  buffer.length > SAVE_DATA_LIMIT / 2 &&
+                  state.settings.saveData
+                ) {
+                  writeSlicedBuffer();
+                } else if (download.status !== 'paused') {
+                  res.resume();
+                }
+              });
+            }, 500);
+          };
+          await writeSlicedBuffer();
         } else {
           fileStream.write(buffer, err => {
             if (err) throw err;
             const received = download.bytesDownloaded + buffer.length;
             buffer = null;
             dispatch(thunkUpdateBytesDownloaded(id, received));
+            
             if (download.status !== 'paused') res.resume();
           });
         }
