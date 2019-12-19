954d1f8d9acdc2e6917a375d85c94a813d2d2f98
Tari Ibaba <tariibabadev@gmail.com>
2019-12-19 20:45:30 +0100

    fix: Remove webpack config module rule

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,9 +1,5 @@
 module.exports = config => {
   config.target = 'electron-renderer';
-  config.module.rules.push({
-    test: /\.(woff|woff2|eot|ttf|otf)$/,
-    use: 'file-loader'
-  });
 
   return config;
 };
