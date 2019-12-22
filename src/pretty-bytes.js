73e476e1e4e69f300f531ef7d5c9ce88c099c22e
Tari Ibaba <tariibabadev@gmail.com>
2019-12-22 19:13:23 +0100

    Adjust logic setting fraction digits

--- src/pretty-bytes.js
+++ src/pretty-bytes.js
@@ -1,7 +1,6 @@
-export default function (bytes) {
-  if (bytes === 0)
-    return '0 B';
-    
+export default function(bytes) {
+  if (bytes === 0) return '0 B';
+
   const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   let unitIndex = 0;
   let prettyBytes = bytes;
@@ -9,7 +8,20 @@ export default function (bytes) {
     prettyBytes /= 1024;
     unitIndex++;
   }
-  const fractionDigits = Math.log10(prettyBytes) < 2 ? 1 : 0;
-  
+
+  const integerDigits = getWholeNumberDigits(prettyBytes);
+  let fractionDigits;
+  if (integerDigits < 2) {
+    fractionDigits = 2;
+  } else if (integerDigits === 2) {
+    fractionDigits = 1;
+  } else {
+    fractionDigits = 0;
+  }
+
   return prettyBytes.toFixed(fractionDigits) + ' ' + units[unitIndex];
+}
+
+function getWholeNumberDigits(number) {
+  return number.toString().split('.')[0].length;
 }
