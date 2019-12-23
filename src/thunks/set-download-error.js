86968b8566c9ff1f8c77fd8fe7cb358c8fb3944c
Tari Ibaba <tariibabadev@gmail.com>
2019-12-24 00:39:15 +0100

    Fix bug causing download error to always be a "File changed" error

--- src/thunks/set-download-error.js
+++ src/thunks/set-download-error.js
@@ -3,7 +3,7 @@ import { setDownloadError, setDownloadRes } from '../actions';
 
 export default function setDownloadErrorThunk(id, error) {
   return dispatch => {
-    dispatch(setDownloadError(id, { code: 'ERR_FILE_CHANGED' }));
+    dispatch(setDownloadError(id, { code: error.code }));
     dispatch(changeDownloadStatusThunk(id, 'error'));
     dispatch(setDownloadRes(id, null));
   };
