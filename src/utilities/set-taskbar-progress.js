0149d77d1984a81eab66f51b886b434bdf157468
Tari Ibaba <tariibabadev@gmail.com>
2020-02-16 17:52:23 +0100

    Animate taskbar progress bar movement

--- src/utilities/set-taskbar-progress.js
+++ src/utilities/set-taskbar-progress.js
@@ -1,7 +1,8 @@
 import { ipcRenderer } from 'electron';
 import sumBy from 'sum-by';
 
-export default function setTaskbarProgress(downloads) {
+let formerTaskbarProgress = 0;
+export default async function setTaskbarProgress(downloads) {
   const activeDownloads = downloads.filter(
     download =>
       download.status === 'notstarted' ||
@@ -11,11 +12,11 @@ export default function setTaskbarProgress(downloads) {
   );
   const activeDownloadsBytesDownloaded = sumBy(
     activeDownloads,
-    download => download.bytesDownloaded
+    download => download.bytesDownloadedShown
   );
   const activeDownloadsSize = sumBy(activeDownloads, download => download.size);
   const activeDownloadsProgress =
-    activeDownloadsBytesDownloaded / activeDownloadsSize || 0;    // assign 0 if NaN
+    activeDownloadsBytesDownloaded / activeDownloadsSize || 0; // assign 0 if NaN
 
   const downloadsIsOnlyGettingInfo =
     activeDownloads.length > 0 &&
@@ -24,11 +25,36 @@ export default function setTaskbarProgress(downloads) {
   if (downloadsIsOnlyGettingInfo) {
     ipcRenderer.send('set-progress-indeterminate');
   } else {
-    ipcRenderer.send('set-progress', activeDownloadsProgress);
+    moveProgressBar(formerTaskbarProgress, activeDownloadsProgress, 500).then(
+      () => {
+        if (activeDownloadsProgress === 1) ipcRenderer.send('clear-progress');
+      }
+    );
+    formerTaskbarProgress = activeDownloadsProgress;
   }
+}
 
-  // If there's just one active download and it's complete
-  if (activeDownloadsProgress === 1) {
-    ipcRenderer.send('clear-progress');
-  }
+function moveProgressBar(from, to, duration) {
+  let lastFrameEndTime = Date.now();
+  const diff = to - from;
+  let curr = from;
+
+  const nextFrame = () => {
+    const scaledDeltaTime = (Date.now() - lastFrameEndTime) / duration;
+    curr += diff * scaledDeltaTime;
+    if (curr > to) curr = to;
+    ipcRenderer.send('set-progress', curr);
+
+    return curr < to;
+  };
+
+  return new Promise(resolve => {
+    const animate = () => {
+      if (nextFrame()) requestAnimationFrame(animate);
+      else resolve();
+      lastFrameEndTime = Date.now();
+    };
+
+    requestAnimationFrame(animate);
+  });
 }
