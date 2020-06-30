8aee8e4d769c9f8487beba4ccc4caebac78315ac
Tari Ibaba <tariibabadev@gmail.com>
2020-06-30 14:50:39 +0100

    Retry a download after any error it can resume from
    Retry after a minute.

--- src/thunks/download-error.js
+++ src/thunks/download-error.js
@@ -1,17 +1,25 @@
 import { showDownloadError } from '../actions';
 import { ipcRenderer } from 'electron';
 import { getDownloadPath } from '../utilities';
+import Timeout from 'await-timeout';
+import resumeDownload from './resume-download';
 
 export default function (id, error) {
-  return (dispatch, getState) => {
+  return async (dispatch, getState) => {
     const download = getState().downloads.byId[id];
     const { code } = error;
-    dispatch(showDownloadError(id, error));
     if (['EFILECHANGED', 'EFORBIDDEN', 'ERANGENOTSATISFIABLE'].includes(code)) {
+      dispatch(showDownloadError(id, error));
       ipcRenderer.send('notify-fail', {
         code,
         filePath: getDownloadPath(download),
       });
+    } else {
+      download.error = error;  // Mutate object to prevent UI change
+      download.res = null;
+      const secondsToWait = 60000;  // One minute
+      await new Timeout().set(secondsToWait);
+      dispatch(resumeDownload(id));
     }
   };
 }
