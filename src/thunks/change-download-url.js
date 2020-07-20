737d6d189701bde39e93dc949285aeecb61120d0
Tari Ibaba <tariibabadev@gmail.com>
2020-07-20 19:56:13 +0100

    Pause download when changing URL if status is also "error"

--- src/thunks/change-download-url.js
+++ src/thunks/change-download-url.js
@@ -4,7 +4,9 @@ import { changeDownloadUrl } from '../actions';
 export default function changeDownloadUrlThunk(id, newUrl) {
   return async (dispatch, getState) => {
     const download = getState().downloads.byId[id];
-    if (download.status === 'progressing') dispatch(pauseDownload(id));
+    if (['progressing', 'error'].includes(download.status)) {
+      dispatch(pauseDownload(id));
+    }
     dispatch(changeDownloadUrl(id, newUrl));
   };
 }
