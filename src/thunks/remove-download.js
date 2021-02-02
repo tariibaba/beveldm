e7cbdae67ba01a19a4f96060e592d5b07b52b933
Tari Ibaba <tariibabadev@gmail.com>
2021-02-02 09:06:35 +0100

    feat: truncate filename in notification
    In notifications about a removed download, truncate the filename of the
    download to 30 characters with ellipses.

--- src/thunks/remove-download.js
+++ src/thunks/remove-download.js
@@ -2,16 +2,20 @@ import { removeDownload, notify, showDownload, hideDownload } from '../actions';
 import { getPartialDownloadPath, deleteFile } from '../utilities';
 import pathExists from 'path-exists';
 
+const REMOVED_DOWNLOAD_NOTIFICATION_FILENAME_TRUNCATION_LENGTH = 30;
+
 export default function removeDownloadThunk(id) {
   return async (dispatch, getState) => {
     dispatch(hideDownload(id));
 
     const download = getState().downloads.byId[id];
-
     dispatch(
       notify({
         type: 'info',
-        message: `Removed '${download.availableFilename}' from list`,
+        message: `Removed '${truncateString(
+          download.availableFilename,
+          REMOVED_DOWNLOAD_NOTIFICATION_FILENAME_TRUNCATION_LENGTH
+        )}' from list`,
         action: 'Undo',
         async responseCallback(responseType) {
           if (responseType === 'undo') {
@@ -29,4 +33,9 @@ export default function removeDownloadThunk(id) {
       })
     );
   };
+}
+
+function truncateString(str, length) {
+  const substring = str.substr(0, length);
+  return `${substring}${(str !== substring && '...') || ''}`;
 }
